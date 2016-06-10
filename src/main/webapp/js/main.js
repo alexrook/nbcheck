1da5381a7bfe63df65ae97f03a4c262aec08f04b
Alex Rook <rook.alex@gmail.com>
2016-06-10 14:46:00 +0300

    start work with index page

--- src/main/webapp/js/main.js
+++ src/main/webapp/js/main.js
@@ -1,2 +1,18 @@
 'use strict';
 
+$(document).ready(function () {
+    $('#mainTable').DataTable({
+        "processing": true,
+        "serverSide": true,
+        ajax: {
+            url: 'rs/nbn',
+            dataSrc: 'row'
+        },
+        "columns": [
+            {"data": "data[0]"},
+            {"data": "data[1]"},
+            {"data": "data[2]"},
+            {"data": "data[3]"}
+        ]
+    });
+});
